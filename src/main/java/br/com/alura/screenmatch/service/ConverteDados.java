7d2be248f4798ee3888f05bde2d1fc81765d4d55
douglas.vieira <douglasbatat@gmail.com>
2025-10-29 21:44:21 -0300

    Inicio Projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import tools.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados{
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T obterdados(String json, Class<T> classe) {
+        return mapper.readValue(json, classe);
+    }
+}
